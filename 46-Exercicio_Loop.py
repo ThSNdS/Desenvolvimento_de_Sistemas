87cc03c9eb5cc2359cb1b4f0e9014b2babcad3b3
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-23 14:59:01 -0300

    Atividades de laço de repetição - for.

---
+++ 46-Exercicio_Loop.py
@@ -0,0 +1,9 @@
+import os
+import time 
+os.system('cls')
+
+for I in range(1,21,2) :
+    print(I)
+    time.sleep(2)
+    
+print('FIM')
---
+++ 47-Exercicio.py
@@ -0,0 +1,9 @@
+import os
+import time
+os.system('cls')
+
+numero = int(input('Digite um numero: '))
+
+for I in range (numero,0,-1):
+    print(I)
+    time.sleep(1)
